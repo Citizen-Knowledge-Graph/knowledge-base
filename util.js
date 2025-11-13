cc8001f2ee0cca49339879f9fd4667297f41b6e9
Benjamin Degenhart <benjamin.degenhart@foerderfunke.org>
2025-11-13 22:45:19 +0100

    Add script to make sh:PropertyShape's unique

---
+++ util.js
@@ -0,0 +1,27 @@
+import path from "path"
+import { fileURLToPath } from "url"
+import { promises } from "fs"
+
+const ROOT = path.join(path.dirname(fileURLToPath(import.meta.url)))
+const shaclDirs = [
+    `${ROOT}/shacl`,
+    `${ROOT}/shacl/beta`,
+    `${ROOT}/shacl/bielefeld`
+]
+let shaclFiles = []
+for (let shaclDir of shaclDirs) {
+    shaclFiles = shaclFiles.concat((await promises.readdir(shaclDir)).map(file => `${shaclDir}/${file}`).filter(file => file.endsWith(".ttl")))
+}
+
+for (let file of shaclFiles) {
+    // let filename = path.basename(file)
+    let content = await promises.readFile(file, "utf8")
+    const cls = "sh:PropertyShape"
+    const regex = new RegExp(`^([^\\s#;]+)\\s+a\\s+${cls}\\b`, "gm")
+    const matches = [... content.matchAll(regex)].map(m => m[1].trim())
+    for (let match of matches) {
+        const hash = Math.random().toString(36).slice(2, 5)
+        content = content.split(match).join(`${match}_${hash}`)
+    }
+    await promises.writeFile(file, content, "utf8")
+}
