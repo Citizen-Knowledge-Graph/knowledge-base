d4bf793f231e3f0b874f8d13ef1aa5f33470715d
Benjamin Degenhart <benjamin.degenhart@foerderfunke.org>
2025-11-14 23:43:37 +0100

    Implement renaming logic

--- archive/new-naming-scheme.js
+++ archive/new-naming-scheme.js
@@ -13,16 +13,64 @@ for (let shaclDir of shaclDirs) {
     shaclFiles = shaclFiles.concat((await promises.readdir(shaclDir)).map(file => `${shaclDir}/${file}`).filter(file => file.endsWith(".ttl")))
 }
 
+const map = {
+    "ff:hilfe-zum-lebensunterhalt": "ff:hlu",
+    "ff:kindergeld": "ff:kdg",
+    "ff:kinderzuschlag": "ff:kzl",
+    "ff:bafoeg": "ff:bfg",
+    "ff:buergergeld": "ff:bgd",
+    "ff:arbeitslosengeld": "ff:alg",
+    "ff:berufsausbildungsbeihilfe-bab": "ff:bab",
+    "ff:wohngeld": "ff:wog",
+    "ff:grundsicherung-im-alter-und-bei-erwerbsminderung": "ff:gsi",
+    "ff:bildung-und-teilhabe-bei-bezug-von-buergergeld": "ff:but",
+    "ff:ausbildungsgeld": "ff:asg",
+    "ff:blindengeld": "ff:blg",
+    "ff:blindenhilfe": "ff:blh",
+    "ff:bundesfoerderung-effiziente-gebaeude": "ff:beg",
+    "ff:exist-gruendungsstipendium": "ff:exs",
+    "ff:forschungszulage": "ff:fzl",
+    "ff:ibb-altersgerecht-wohnen": "ff:iaw",
+    "ff:ibb-gruendungsbonus": "ff:igb",
+    "ff:ifb-gruendung-nachfolge": "ff:ifn",
+    "ff:ilb-kredit-gruendung": "ff:ilg",
+    "ff:kurzzeitige-arbeitsverhinderung": "ff:kav",
+    "ff:uebergangsgeld": "ff:ueg",
+    "ff:wolfenbuettel-ansiedlung": "ff:wfa",
+    "ff:wolfenbuettel-gastronomische-aussenmoeblierung": "ff:wfgam",
+    "ff:wolfenbuettel-stiftung-organisation": "ff:wfsto",
+    "ff:wolfenbuettel-stiftung-person": "ff:wfstp",
+    "ff:allgemeine-krankenversicherungspflicht": "ff:akv",
+    "ff:arbeitsassistenz": "ff:aas",
+    "ff:eingliederungshilfe": "ff:egh",
+    "ff:erwerbsminderungsrente-teilweise": "ff:emt",
+    "ff:erwerbsminderungsrente-voll": "ff:emv",
+    "ff:persoenliches-budget": "ff:pbg",
+    "ff:schwerbehindertenausweis": "ff:sba",
+    "ff:substitutionstherapie-diamorphin": "ff:std",
+    "ff:substitutionstherapie-regulaer": "ff:str"
+}
+
 for (let file of shaclFiles) {
     // let filename = path.basename(file)
     let content = await promises.readFile(file, "utf8")
-    const cls = "ff:RequirementProfile"
-    const regex = new RegExp(`^([^\\s#;]+)\\s+a\\s+${cls}\\b`, "gm")
+
+    let cls = "ff:RequirementProfile"
+    let regex = new RegExp(`(.*?)\\s+a\\s+${cls}`)
+    const oldRpUri = content.match(regex)[1].trim()
+    const newRpUri = map[oldRpUri]
+    content = content.split(oldRpUri + " a ").join(newRpUri + " a ")
+    content = content.split(oldRpUri + "MainShape").join(newRpUri + "MainShape")
+    content = content.split(oldRpUri + "FlowShape").join(newRpUri + "FlowShape")
+
+    cls = "sh:PropertyShape"
+    regex = new RegExp(`^([^\\s#;]+)\\s+a\\s+${cls}\\b`, "gm")
     const matches = [... content.matchAll(regex)].map(m => m[1].trim())
-    console.log(matches[0])
-    /*for (let match of matches) {
-        const hash = Math.random().toString(36).slice(2, 5)
-        content = content.split(match).join(`${match}_${hash}`)
+    for (let oldPsUri of matches) {
+        let localName = oldPsUri.split(":")[1]
+        let newPsUri = newRpUri + localName.charAt(0).toUpperCase() + localName.slice(1)
+        content = content.split(oldPsUri).join(newPsUri)
     }
-    await promises.writeFile(file, content, "utf8")*/
+
+    await promises.writeFile(file, content, "utf8")
 }
